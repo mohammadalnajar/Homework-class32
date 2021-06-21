3ca0aabefcc452b3da44b936515d8a58a432841d
Mohammad <malnajjar16@gmail.com>
2021-06-21 13:40:37 +0200

    ex2-solved

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -20,18 +20,67 @@ Use async/await and try/catch to handle promises.
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
-function fetchData(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+const getDataBtnClick = async () => {
+  const data = await fetchData('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const pokemons = data.results;
+  fetchAndPopulatePokemons(pokemons);
+};
+
+const selectOption = async (e) => {
+  const data = await fetchData('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const pokemons = data.results;
+  pokemons.forEach(async (pokemon) => {
+    if (pokemon.name == e.target.value) {
+      const data = await fetchData(pokemon.url);
+      fetchImage(data.sprites.front_default);
+    }
+  });
+};
+
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function fetchAndPopulatePokemons(pokemons) {
+  const selectList = document.querySelector('.select-list');
+  selectList.innerHTML = '';
+  const option = document.createElement('option');
+  option.textContent = 'Select one Pokemon';
+  selectList.appendChild(option);
+  pokemons.forEach((pokemon, i) => {
+    const option = document.createElement('option');
+    option.value = pokemon.name;
+    option.textContent = `${i + 1} ${pokemon.name}`;
+    selectList.appendChild(option);
+  });
 }
 
-function fetchImage(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function fetchImage(data) {
+  const img = document.createElement('img');
+  const imagesContainer = document.querySelector('.images-container');
+  imagesContainer.innerHTML = '';
+  img.src = data;
+  imagesContainer.appendChild(img);
 }
 
 function main() {
-  // TODO complete this function
+  const getDataBtn = document.createElement('button');
+  const selectList = document.createElement('select');
+  const imagesContainer = document.createElement('div');
+  imagesContainer.classList.add('images-container');
+  selectList.classList.add('select-list');
+  getDataBtn.setAttribute('type', 'submit');
+  getDataBtn.textContent = 'Get Pokemon';
+  document.body.appendChild(getDataBtn);
+  document.body.appendChild(selectList);
+  document.body.appendChild(imagesContainer);
+  getDataBtn.addEventListener('click', getDataBtnClick);
+  selectList.addEventListener('change', selectOption);
 }
+window.addEventListener('load', main);
